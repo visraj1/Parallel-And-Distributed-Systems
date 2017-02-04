9b89619d0b7cd26fff855ecc2e1f538ac91fb968
Vishnu Suresh <vishnusraju@gmail.com>
2017-02-04 10:53:56 +0100

    git commit

---
+++ Quicksort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class QuickSort{
+
+public static void main(String[] args){
+
+    int[] number=new int[10];
+    Scanner sc=new Scanner(System.in);
+    for(int i=0;i<5;++i)
+    {
+        number[i]=sc.nextInt();
+    }   
+    
+
+
+}
+
+}
+
